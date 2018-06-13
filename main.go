23c6068fb848018d094bd5395367ee8fbecaec40
Camal Cakar <camal_cakar@yahoo.de>
2018-06-13 23:25:44 +0200

    Displaying spacelines

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 )
 
 func main() {
-	n, _ := fmt.Printf("%-15v %-4v %-10v %-5v\n", "Spaceline", "Days", "Round-trip", "Price")
+	//var distance = 62100000
+
+	n, _ := fmt.Printf("%-20v %-5v %-10v %-5v\n", "Spaceline", "Days", "Round-trip", "Price")
 	line := strings.Repeat("=", n)
 	fmt.Println(line)
+
+	for i := 0; i < 10; i++ {
+		airline := rand.Intn(3) + 1
+
+		switch airline {
+		case 1:
+			fmt.Printf("%-20v\n", "Space Adventures")
+		case 2:
+			fmt.Printf("%-20v\n", "SpaceX")
+		case 3:
+			fmt.Printf("%-20v\n", "Virgin Galactic")
+		}
+	}
 }
